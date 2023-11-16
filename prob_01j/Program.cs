cacf95a9ec8fab0a3bedb8f96ae6cdb192c17784
Usuario <Usuario@DESKTOP-U4P8J7O>
2023-11-16 18:57:50 -0500

    Agregar archivos de proyecto.

---
+++ prob_01j/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_01j
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int km = 0;
+            string fruta;
+            int precio = 0;
+
+            Console.WriteLine("escriba el tipo de fruta a llevar:");
+            fruta = Console.ReadLine();
+            Console.WriteLine("ingrese el numero de kilometros que debe recorrer:");
+            km = int.Parse(Console.ReadLine());
+            switch(fruta)
+            {
+                case "naranja":
+                    if(km < 30)
+                    {
+                        precio = 2;
+                    }
+                    else 
+                    {
+                        precio = 4;
+                    }
+                break;
+
+                case "fresa":
+                    if(km < 30)
+                    {
+                        precio = 6;
+                    }
+                    else
+                    {
+                        precio = 9;
+                    }
+                break;
+
+                case "mango":
+                    if (km < 30)
+                    {
+                        precio = 3;
+                    }
+                    else
+                    {
+                        precio = 5;
+                    }
+                break;   
+                    
+            }
+            Console.WriteLine("el precio de la fruta escogida es:");
+            Console.WriteLine(precio);
+            Console.ReadKey();
+        }
+    }
+}
